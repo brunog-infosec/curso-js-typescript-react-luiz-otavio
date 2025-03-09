8beadd6d66d55038fa5a05ab035b779c5d01db74
Bruno Garcia <brunog.infosec@proton.me>
2025-03-09 09:51:49 -0300

    incluido aula 58 - funcao construtora

---
+++ aula58-funcao-construtora/aula58.js
@@ -0,0 +1,30 @@
+// Função Construtura -> retorna objetos
+// Função fabrica -> retorna objetos
+// Construtora -> Pessoa (new)
+
+function Pessoa(nome, sobrenome) {
+  //Atributos ou métodos públicos
+  this.nome = nome;
+  this.sobrenome = sobrenome;
+
+  //atributos ou métodos privados
+  const ID = 12345;
+  const metodoInterno = function () {
+    console.log(`Id do usuário : ${ID}`);
+  };
+
+  //Metodo
+  this.metodo = function () {
+    console.log("Sou um metodo do " + this.nome);
+    metodoInterno();
+  };
+}
+
+const p1 = new Pessoa("Carlos", "Oliveira");
+const p2 = new Pessoa("Marina", "Joaquina");
+
+// new -> cria um novo objeto vazio, a faz o this apontar para o objeto criado e faz o retorno
+console.log(p1.nome, p1.sobrenome);
+console.log(p2.nome, p2.sobrenome);
+p1.metodo();
+p2.metodo();
