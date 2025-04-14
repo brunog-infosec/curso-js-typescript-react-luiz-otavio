e4c1dfbcd5074ec473fff89b01092817236127d9
Bruno Garcia <bruno_garcia_ig@hotmail.com>
2025-04-14 11:53:40 -0300

    uploaded aula 11

---
+++ Node/aula11-static-pages/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+//Importação das Rotas
+const routes = require("./routes");
+const path = require("path");
+
+// tratamento do body das requisições pelo node expresss é obrigatório utilizar essa função
+app.use(express.urlencoded({ extended: true }));
+
+//Referenciando o caminho do static pages
+app.use(express.static(path.resolve(__dirname, "public")));
+
+// Configuração do Caminho da pasta views
+app.set("views", path.resolve(__dirname, "src", "views"));
+// Configuração da engine que será utilizada para views
+app.set("view engine", "ejs");
+
+// Use do express Router
+app.use(routes);
+
+// Fazendo o servidor ficar ouvindo a porta http
+app.listen(3000, () => {
+  console.log("Acessar http://localhost:3000");
+  console.log("Servidor executando na porta 3000");
+});
