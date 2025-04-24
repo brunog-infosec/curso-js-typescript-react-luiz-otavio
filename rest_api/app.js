ba85f9a03fbe7169333b18e09fb67c7d30633498
Bruno Garcia <bruno_garcia_ig@hotmail.com>
2025-04-24 15:33:27 -0300

    feat: adiciona estrutura inicial de rotas e implementa homeController com rota GET /

---
+++ rest_api/app.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import homeRoutes from './src/routes/homeRoutes';
+
+class App {
+  constructor() {
+    this.app = express();
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+    this.app.use(express.urlencoded({ extended: true }));
+    this.app.use(express.json());
+  }
+
+  routes() {
+    this.app.use('/', homeRoutes);
+    // this.app.use('/', (req, res) => {
+    //   res.send('Olá mundo');
+    // });
+  }
+}
+
+export default new App().app;
