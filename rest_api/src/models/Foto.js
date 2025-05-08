3d7892e81e13fcf189e8322ffdc004d63bc21aab
Bruno Garcia <bruno_garcia_ig@hotmail.com>
2025-05-08 14:32:22 -0300

    Aula 185 - Criando Tabela Fotos e salvando foto no banco de dados

---
+++ rest_api/src/models/Foto.js
@@ -0,0 +1,35 @@
+import Sequelize, { Model } from 'sequelize';
+
+export default class Foto extends Model {
+  static init(sequelize) {
+    super.init({
+      originalname: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validate: {
+          notEmpty: {
+            msg: 'Campo não pode ficar vazio.',
+          },
+        },
+      },
+      filename: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validate: {
+          notEmpty: {
+            msg: 'Campo não pode ficar vazio.',
+          },
+        },
+      },
+    }, {
+      sequelize,
+      tableName: 'fotos',
+    });
+    return this;
+  }
+
+  // Realiza a associação da tabela de foto com a tabela de Alunos pela chave aluno_id
+  static associate(models) {
+    this.belongsTo(models.Aluno, { foreignKey: 'aluno_id' });
+  }
+}
