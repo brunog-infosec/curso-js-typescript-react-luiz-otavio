77d9a85da161a86aa350d0639ec147408a54e6ea
Bruno Garcia <bruno_garcia_ig@hotmail.com>
2025-05-15 14:08:11 -0300

    update: Header da aplicação e react-icons

--- REACT/react-base/src/pages/Login/index.js
+++ REACT/react-base/src/pages/Login/index.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React from 'react';
 import { Title, Paragrafo } from './styled';
-import { Container } from "../../../styles/GlobalStyles";
+import { Container } from '../../styles/GlobalStyles';
 
 export default function Login() {
   return (
