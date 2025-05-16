ca4c2ccb815e05cc924b9e89c45a1cba4ccde566
Bruno Garcia <bruno_garcia_ig@hotmail.com>
2025-05-16 11:00:11 -0300

    aula 214: configuração axios

--- REACT/react-base/src/pages/Login/index.js
+++ REACT/react-base/src/pages/Login/index.js
@@ -1,19 +1,8 @@
 import React from 'react';
 import { Title, Paragrafo } from './styled';
 import { Container } from '../../styles/GlobalStyles';
-import { toast } from 'react-toastify';
 
 export default function Login() {
-  toast.success('Login feito com Sucesso', {
-    toastId: 'successId',
-    theme: 'dark',
-  });
-
-  toast.error('Login ou senha inválidos', {
-    toastId: 'errorId',
-    theme: 'dark',
-  });
-
   return (
     <Container>
       <Title>Página de Login</Title>
---
+++ REACT/react-base/src/services/axios.js
@@ -0,0 +1,6 @@
+import axios from 'axios';
+
+//configuração da url padrão para requisições axios
+export default axios.create({
+  baseURL: 'http://34.39.144.113:81',
+});
