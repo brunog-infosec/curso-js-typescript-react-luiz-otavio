32a5373a332419d5a0e6fcaa1ebf0a30e579186d
Bruno Garcia <bruno_garcia_ig@hotmail.com>
2025-05-16 09:34:39 -0300

    importação react-toastify e teste de uso na página de login

--- REACT/react-base/src/pages/Login/index.js
+++ REACT/react-base/src/pages/Login/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import { Title, Paragrafo } from './styled';
 import { Container } from '../../styles/GlobalStyles';
+import { toast } from 'react-toastify';
 
 export default function Login() {
+  toast.success('Login feito com Sucesso', {
+    toastId: 'successId',
+    theme: 'dark',
+  });
+
+  toast.error('Login ou senha inválidos', {
+    toastId: 'errorId',
+    theme: 'dark',
+  });
+
   return (
     <Container>
-      <Title>
-        Página de Login
-        <small>Bom dia</small>
-      </Title>
-      <Paragrafo>Lorem ipsum dolor sit amet</Paragrafo>
+      <Title>Página de Login</Title>
+      <Paragrafo>Sistema de Alunos 1.0</Paragrafo>
     </Container>
   );
 }
