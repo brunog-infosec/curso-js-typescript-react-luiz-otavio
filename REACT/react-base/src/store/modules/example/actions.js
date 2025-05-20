c165fd222f7bfb350a428bc4c160cedabd93f2e2
Bruno Garcia <bruno_garcia_ig@hotmail.com>
2025-05-20 17:00:07 -0300

    aula 217: configuracao redux-saga e exemplo de configuração

--- REACT/react-base/src/store/modules/example/actions.js
+++ REACT/react-base/src/store/modules/example/actions.js
@@ -1,8 +1,20 @@
 //Responsável por disparar as ações
 import * as types from '../types';
 
-export function clicaBotao() {
+export function clicaBotaoRequest() {
   return {
-    type: types.BOTAO_CLICADO,
+    type: types.BOTAO_CLICADO_REQUEST,
+  };
+}
+
+export function clicaBotaoSuccess() {
+  return {
+    type: types.BOTAO_CLICADO_SUCCESS,
+  };
+}
+
+export function clicaBotaoFailure() {
+  return {
+    type: types.BOTAO_CLICADO_FAILURE,
   };
 }
--- REACT/react-base/src/store/modules/example/reducer.js
+++ REACT/react-base/src/store/modules/example/reducer.js
@@ -1,13 +1,26 @@
+import * as types from '../types';
+
 const initialState = {
   botaoClicado: false,
 };
 //reducer: escutar as ações que estão sendo disparadas, em um estado global da aplicação
-export default function (state = initialState, action) {
+export default function exampleReducer(state = initialState, action) {
   switch (action.type) {
-    case 'BOTAO_CLICADO': {
+    case types.BOTAO_CLICADO_SUCCESS: {
+      console.log('Sucesso =)');
       const newState = { ...state };
       newState.botaoClicado = !newState.botaoClicado;
       return newState;
+    }
+
+    case types.BOTAO_CLICADO_REQUEST: {
+      console.log('Estou fazendo a requisição');
+      return state;
+    }
+
+    case types.BOTAO_CLICADO_FAILURE: {
+      console.log('Falhou ...');
+      return state;
     }
 
     default: {
