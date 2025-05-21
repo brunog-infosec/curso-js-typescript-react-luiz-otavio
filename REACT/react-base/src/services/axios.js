2172b0ea8c679dcb64370f22292aad9e5816bd44
Bruno Garcia <bruno_garcia_ig@hotmail.com>
2025-05-21 14:03:59 -0300

    removida url do axios

--- REACT/react-base/src/services/axios.js
+++ REACT/react-base/src/services/axios.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 //configuração da url padrão para requisições axios
 export default axios.create({
-  baseURL: 'http://34.39.144.113:81',
+  baseURL: 'COLOQUE_SUA_URL_AQUI',
 });
