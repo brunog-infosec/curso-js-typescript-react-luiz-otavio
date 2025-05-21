9e049390c3163c041b237ffcb3f50d891d089a96
Bruno Garcia <bruno_garcia_ig@hotmail.com>
2025-05-21 13:44:29 -0300

    aula 218: configuração redux persist

---
+++ REACT/react-base/src/store/modules/reduxPersist.js
@@ -0,0 +1,17 @@
+import storage from 'redux-persist/lib/storage';
+// uma biblioteca para salvar os dados persistentes no local storage
+import { persistReducer } from 'redux-persist';
+// Uma maneira de combinar os reducers
+
+export default (reducers) => {
+  const persistedReducers = persistReducer(
+    {
+      key: 'NOME_DA_APLICACAO',
+      storage,
+      whitelist: ['example'], // Qual modulo vai salvar
+    },
+    reducers,
+  );
+
+  return persistedReducers;
+};
