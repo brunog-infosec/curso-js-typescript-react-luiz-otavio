06c0f3945b9c2cdc8cea16bc590a667e82a8805e
Bruno Garcia <bruno_garcia_ig@hotmail.com>
2025-05-21 17:04:53 -0300

    aula 220: configuração das paginas e rotas pricipais

--- REACT/react-api/src/pages/Page404/index.js
+++ REACT/react-api/src/pages/Page404/index.js
@@ -1,10 +1,9 @@
-import React from 'react';
 import { Container } from '../../styles/GlobalStyles';
 
 export default function Page404() {
   return (
     <Container>
-      <h1>Essa página não existe</h1>
+      <h1>ERRO 404 - Essa página não existe</h1>
     </Container>
   );
 }
--- REACT/react-api/src/routes/index.js
+++ REACT/react-api/src/routes/index.js
@@ -3,13 +3,22 @@ import { Switch } from 'react-router-dom';
 import MyRoute from './MyRoute';
 
 import Login from '../pages/Login';
+import Aluno from '../pages/Aluno';
+import Alunos from '../pages/Alunos';
+import Register from '../pages/Register';
+import Fotos from '../pages/Fotos';
 import Page404 from '../pages/Page404';
 
 export default function Routes() {
   return (
     <Switch>
-      <MyRoute exact path="/" component={Login} />
-      <MyRoute path="*" component={Page404} />
+      <MyRoute exact path="/" component={Alunos} isClosed={false} />
+      <MyRoute path="/aluno/:id/edit" component={Aluno} isClosed />
+      <MyRoute path="/aluno" component={Aluno} isClosed />
+      <MyRoute path="/fotos/:id" component={Fotos} isClosed />
+      <MyRoute path="/register/" component={Register} />
+      <MyRoute path="/login/" component={Login} isClosed={false} />
+      <MyRoute path="*" component={Page404} isClosed={false} />
     </Switch>
   );
 }
