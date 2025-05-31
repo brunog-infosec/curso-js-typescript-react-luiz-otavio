78ac64659712597cbb42d4ce6bbc769a59aa5785
Bruno Garcia <bruno_garcia_ig@hotmail.com>
2025-05-31 11:02:38 -0300

    aula 224: autenticação do usuário logado e persistencia dos dados localmente

--- REACT/react-api/src/store/modules/auth/reducer.js
+++ REACT/react-api/src/store/modules/auth/reducer.js
@@ -9,9 +9,18 @@ const initialState = {
 //reducer: escutar as ações que estão sendo disparadas, em um estado global da aplicação
 export default function exampleReducer(state = initialState, action) {
   switch (action.type) {
-    case types.LOGIN_REQUEST: {
-      console.log('REDUCER', action.payload);
-      return state;
+    case types.LOGIN_SUCCESS: {
+      // console.log('REDUCER', action.payload);
+      const newState = { ...state };
+      newState.isLoggedIn = true;
+      newState.token = action.payload.token;
+      newState.user = action.payload.user;
+      return newState;
+    }
+
+    case types.LOGIN_FAILURE: {
+      const newState = { ...initialState };
+      return newState; // caso houver falha no login, desloga o usuário enviando o estado inicial
     }
 
     default: {
--- REACT/react-api/src/store/modules/auth/sagas.js
+++ REACT/react-api/src/store/modules/auth/sagas.js
@@ -1,10 +1,37 @@
 import { call, put, all, takeLatest } from 'redux-saga/effects';
 import { toast } from 'react-toastify';
+import { get } from 'lodash';
+
 import * as actions from './actions';
 import * as types from '../types';
+import axios from '../../../services/axios';
+import history from '../../../services/history';
 
 function* loginRequest({ payload }) {
-  console.log('SAGA', payload);
+  try {
+    // const { email, password } = payload;
+    const response = yield call(axios.post, '/tokens', payload);
+    yield put(actions.loginSuccess({ ...response.data }));
+    toast.success('Login efetuado com sucesso');
+
+    axios.defaults.headers.Authorization = `Bearer ${response.data.token}`; // pega o token e envia no cabeçalho de authorization
+
+    history.push(payload.prevPath); // Redireciona o usuário para a página anterior
+  } catch (e) {
+    toast.error('Usuário ou senha inválidos');
+    yield put(actions.loginFailure());
+  }
 }
 
-export default all([takeLatest(types.LOGIN_REQUEST, loginRequest)]);
+// Rehydrate mantém os dados no cabeçalho da aplicação mesmo depois de atualizar a página
+function persistRehydrate({ payload }) {
+  const token = get(payload, 'auth.token');
+  if (!token) return;
+
+  axios.defaults.headers.Authorization = `Bearer ${token}`; // pega o token e envia no cabeçalho de authorization
+}
+
+export default all([
+  takeLatest(types.LOGIN_REQUEST, loginRequest),
+  takeLatest(types.PERSIST_REHYDRATE, persistRehydrate),
+]);
